28886a5d8ee0f4466e9467ad39362206e47f5a78
chaudhariGaurav07 <gauravchaudhari7717@gmail.com>
2025-07-29 23:03:40 +0530

    create user model with ApiError and ApiResponce Utilities

---
+++ src/middlewares/auth.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+
+export const requireAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findByid(decoded.id).select("-password");
+    next();
+  } catch (err) {
+    throw new ApiError(401, "Invalid token");
+  }
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true 
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true 
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    role: {
+    type: String,
+    default: "admin"
+  }
+})
+
+// Hash password before saving
+ userSchema.pre("save", async function (next) {
+    if(!this.isModified("password"))
+        return next();
+    this.password = await bcrypt.hash(this.password,10)
+    next();
+ })
+
+//comapre password method
+userSchema.methods.comparePassword =  async function (plainPassword) {
+    return await bcrypt.compare(plainPassword, this.password)
+};
+
+export const user = mongoose.model("User", userSchema);
